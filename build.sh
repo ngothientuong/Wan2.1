bb0dad4faa484b5da33a3194348eba80e5419b13
Tuong Ngo <tuongdevops1@gmail.com>
2025-03-16 01:27:17 -0500

    Addedd time out to 4 hours instead of default 1 hour

--- build.sh
+++ build.sh
@@ -10,7 +10,8 @@ azurelogin
 # Build the Docker image with the specified version
 az acr build --registry tngodemo1cr \
     --image ttv/wan21/wan2.1:$VERSION \
-    --file Dockerfile .
+    --file Dockerfile \
+    --timeout 14400 .  # Increase timeout to 4 hours and specify current directory
 
 # Print a message indicating the build is complete
 echo "Docker image built with version $VERSION"
