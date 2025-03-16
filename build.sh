3686977b7fb6e74008b81fd0b0ad47924661fead
Tuong Ngo <tuongdevops1@gmail.com>
2025-03-16 00:51:42 -0500

    update build.sh to build at Azure

--- build.sh
+++ build.sh
@@ -8,7 +8,9 @@ source ${HOME}/ngo/projects/personal/.env_tngo-ai-svcs
 source ${HOME}/.bashrc
 azurelogin
 # Build the Docker image with the specified version
-docker build -t tngodemo1cr.azurecr.io/ttv/wan21/wan2.1:$VERSION .
+az acr build --registry tngodemo1cr \
+    --image ttv/wan21/wan2.1:$VERSION \
+    --file Dockerfile .
 
 # Print a message indicating the build is complete
 echo "Docker image built with version $VERSION"
